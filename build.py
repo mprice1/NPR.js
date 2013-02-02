7a0b6915d19e2cc885f2ca992bc2c94da757559f
Michael Price <mbprice11@gmail.com>
2013-02-02 18:26:04 -0500

    simple build script to concat files

--- build.py
+++ build.py
@@ -1,2 +1,29 @@
-if __name__ == '__main_':
-    build()
+src_root = "src/"
+outfile = "npr_build.js"
+
+import os
+
+def get_sources():
+  srcs = []
+  for (path, dirs, files) in os.walk(src_root):
+    for file in files:
+      norm_path = os.path.normpath(os.path.join(path, file))
+      if os.path.splitext(norm_path)[1] == ".js":
+      	srcs.append(norm_path)
+  return srcs
+
+def concat_sources(srcs):
+  concat = ""
+  for src in srcs:
+    concat += "//////////////////////////////////////\n"
+    concat += "// " + src + "\n"
+    concat += "//////////////////////////////////////\n"
+    concat += open(src, 'r').read()
+    concat += "\n"
+  return concat
+
+def build():
+  open(outfile, 'w').write(concat_sources(get_sources()));
+
+if __name__ == '__main__':
+  build()
